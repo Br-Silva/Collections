17b16ab67cf8e19fee3bb9d5b78ebdc139df1ea4
Br-Silva <br_hxcx@hotmail.com>
2022-07-04 18:12:37 -0300

    Adicionando a função comportamentosMap e atualizando o arquivo testaMap

--- src/main/kotlin/br/com/alura/Main.kt
+++ src/main/kotlin/br/com/alura/Main.kt
@@ -1,50 +1,23 @@
-import br.com.alura.testaMap
-
 fun main() {
-    val pedidos: MutableMap<Int, Double> = mutableMapOf(
-        Pair(1, 27.0),
-        Pair(2, 68.0),
-        3 to 52.0,
-        4 to 70.0,
-        5 to 102.0,
-        6 to 90.0
+    val pedidos = listOf(
+        Pedido(1, 20.0),
+        Pedido(2, 90.0),
+        Pedido(3, 45.0),
+        Pedido(4, 60.0)
     )
-    //    val valorPedido = pedidos.getValue(5)
-//    println(valorPedido)
-    val mensagem: Double? = pedidos.getOrElse(0, {
-        0.0
-    })
-    println(mensagem)
-    println(pedidos.getOrDefault(1, -1.0))
-    println(pedidos.getOrDefault(0, -1.0))
+    println(pedidos)
+    val pedidosMapeados: Map<Int, Pedido> = pedidos.associateBy { pedido ->
+        pedido.numero
+    }
+    println(pedidosMapeados)
+    println(pedidosMapeados[1])
 
-    println(pedidos.keys)
-
-    for (numero in pedidos.keys){
-        println("Pedido $numero")
+    val pedidosFreteGratis: Map<Pedido, Boolean> = pedidos.associateWith { pedido ->
+        pedido.valor > 50.0
+    }
+    println(pedidosFreteGratis)
+    println(pedidosFreteGratis[Pedido(numero = 2, valor = 60.0)])
     }
 
-    println(pedidos.values)
-
-    for (valor in pedidos.values){
-        println("Valor do pedido: $valor")
-    }
-
-    val pedidosFiltrados = pedidos.filter { (numero, valor) ->
-        numero % 2 == 0 && valor > 70.0
-    }
-
-    println(pedidosFiltrados)
-
-    val pedidosAcima = pedidos.filterValues { valor ->
-        valor > 70.0
-    }
-    println(pedidosAcima)
-
-    val pedidosPares = pedidos.filterKeys { numero ->
-        numero % 2 == 0
-    }
-    println(pedidosPares)
-}
-
+data class Pedido(val numero: Int, val valor: Double)
 
